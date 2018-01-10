7929b0d36d01cede8c7d6c0a95eaf6ada4da0e68
einavram <einavr@post.bgu.ac.il>
2018-01-10 18:17:02 +0200

    update

---
+++ UI/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const bodyParser = require('body-parser')
+const {HttpReq} = require('./scripts/api.js');
+const formula = require('./routes/formula')(__dirname);
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.use('/formula',formula)
+
+
+app.get('/admin', function(req, res) {
+    res.sendFile(path.join(__dirname + '/admin.html'));
+});
+
+app.post('/compute', function(req, res) {
+    HttpReq(req.body).then(data => res.send(data))
+});
+
+app.listen(process.env.PORT ? process.env.PORT : 3000, () => console.log('Example app listening on port 3000!'))
