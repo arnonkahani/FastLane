27040aeceab87ac740358b92675d762fd935fe7d
einavram <einavr@post.bgu.ac.il>
2018-04-29 17:06:26 +0300

    updated visualizations

--- UI/public/analytics.js
+++ UI/public/analytics.js
@@ -15,12 +15,17 @@ function clickWrapper(button){
 
 window.Einav = {name:"Einav",clickWrapper:clickWrapper}
 
-{/* <a id="demo"></a> */}
-
-
-// document.onmousemove = function(event){
-//     var x = event.clientX;
-//     var y = event.clientY; 
-//     var coor = "X coords: " + x + ", Y coords: " + y;
-//     document.getElementById("demo").innerHTML = coor;
+// html2canvas(document.body).then(function (canvas) {
+//     document.getElementById("capture").appendChild(canvas);
+//     document.onmousemove = function (event) {
+//         var x = event.clientX * (document.getElementById("capture").childNodes[0].width / document.documentElement.scrollWidth);
+//         var y = event.clientY * (document.getElementById("capture").childNodes[0].height / document.documentElement.scrollHeight);
+//         var coor = "X coords: " + x + ", Y coords: " + y;
+//         var canvas = document.getElementById("capture").childNodes[0];
+//         var ctx = canvas.getContext('2d');
+//         ctx.fillStyle = 'rgba(0, 0, 200, 0.1)';
+//         ctx.fillRect(x, y, 5, 5);
 //     }
+
+
+// });
